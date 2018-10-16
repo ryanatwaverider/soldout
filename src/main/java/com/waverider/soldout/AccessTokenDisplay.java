cbdfa934f932e0f928b14f52e3b71eb24295b86d
ryanatwaverider <ryan@waveridersoftware.com>
2018-10-16 13:31:13 -0500

    More gui

---
+++ src/main/java/com/waverider/soldout/AccessTokenDisplay.java
@@ -0,0 +1,14 @@
+package com.waverider.soldout;
+
+public class AccessTokenDisplay {
+	
+	public Long value;
+	
+	public int ownerId;
+
+	public boolean isListing;
+	
+	public AccessTokenDisplay() {
+		
+	}
+}
